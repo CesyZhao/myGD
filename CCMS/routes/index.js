a5138da39f19f03d29b43a1648de3f9bcf367bfc
cesy <cesyzhao@163.com>
2017-02-28 17:18:37 +0800

    'GD_day02'

---
+++ CCMS/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: '大学生社团活动平台' });
+});
+
+module.exports = router;
