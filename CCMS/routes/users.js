1312f6955beb9499df42985f12f075cd0f727856
cesy <cesyzhao@163.com>
2017-03-03 16:55:48 +0800

    'day03'

--- CCMS/routes/users.js
+++ CCMS/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
-
+var pool = require("customer/pool");
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
   res.render("login");
 });
-
+router.get('/register', function(req, res, next) {
+    res.render("register");
+});
+router.get('/personal', function(req, res, next) {
+    res.render("personal");
+});
 module.exports = router;
